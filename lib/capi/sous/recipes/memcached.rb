c28f54fe60d14f5de7e7f3bc1b10696544da0745
Paul Alexander <paul@xheo.com>
2012-11-28 11:00:44 -0800

    Define roles as array

--- lib/capi/sous/recipes/memcached.rb
+++ lib/capi/sous/recipes/memcached.rb
@@ -4,13 +4,13 @@ Capistrano::Configuration.instance.load do
 
   namespace :memcached do
     desc "Install Memcached"
-    task :install, roles: :app do
+    task :install, roles: [:app] do
       run "#{sudo} apt-get install memcached"
     end
     after "deploy:install", "memcached:install"
 
     desc "Setup Memcached"
-    task :setup, roles: :app do
+    task :setup, roles: [:app] do
       template "memcached.erb", "/tmp/memcached.conf"
       run "#{sudo} mv /tmp/memcached.conf /etc/memcached.conf"
       restart
@@ -19,7 +19,7 @@ Capistrano::Configuration.instance.load do
 
     %w[start stop restart].each do |command|
       desc "#{command} Memcached"
-      task command, roles: :app do
+      task command, roles: [:app] do
         run "#{sudo} service memcached #{command}"
       end
     end
