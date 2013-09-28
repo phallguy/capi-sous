0970a4d1ec3ac3efeeb1f2fdc9771fdd216055e7
Paul Alexander <paul@xheo.com>
2013-09-28 11:11:23 -0700

    Move assets symlink to assets recipie

--- lib/capi/sous/recipes/assets.rb
+++ lib/capi/sous/recipes/assets.rb
@@ -4,3 +4,15 @@ require 'capistrano/fanfare'
 Capistrano::Configuration.instance.load do
   fanfare_recipe 'assets'
 end
+
+namespace :deploy do
+  namespace :assets do
+    task :force_asset_symlink, roles: :app, except: { no_release: true } do
+      if repository
+        # deploy.assets.symlink
+      end
+    end
+    # Force a symlink of the assets immediately after updating the code cause git deletes everything on update code
+    before "deploy:finalize_update", "deploy:assets:force_asset_symlink"
+  end  
+end
--- lib/capi/sous/recipes/release.rb
+++ lib/capi/sous/recipes/release.rb
@@ -9,14 +9,7 @@ namespace :deploy do
       run "cd #{current_path} && git rev-parse --short HEAD > REVISION"
     end
     after "deploy:finalize_update", "deploy:release:mark"
-
-    task :force_asset_symlink, roles: :app, except: { no_release: true } do
-      if repository
-        deploy.assets.symlink
-      end
-    end
-    # Force a symlink of the assets immediately after updating the code cause git deletes everything on update code
-    before "deploy:finalize_update", "deploy:release:force_asset_symlink"
+    
 
   end
 end
